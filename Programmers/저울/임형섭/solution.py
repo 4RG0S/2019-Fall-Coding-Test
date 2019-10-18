87bbec6d96b8fb52698af02376db908b63e8e04d
hyeongseoblim <iii3@cnu.ac.kr>
2019-10-18 19:28:09 +0900

    add to file

--- Programmers/저울/임형섭/solution.py
+++ Programmers/저울/임형섭/solution.py
@@ -1,34 +1,20 @@
 
 # coding: utf-8
 
-# In[58]:
+# In[13]:
 
 
-
-def solution(record):
-    answer = []
-    user_list = dict()
-    log_list= list()
-    answer1 =list()
-    for line in record:
-        split_line = line.split(" ")
-        if split_line[0] == "Enter":
-            user_list[split_line[1]]=split_line[2]
-            log_list.append((split_line[0],split_line[1]))
-        elif split_line[0] == "Leave":
-            log_list.append((split_line[0],split_line[1]))
+def solution(weight):
+    answer = 0
+    weight = sorted(weight)
+    answer= weight[0]
+    sol_list = list()
+    for i in range(len(weight)):
+        if answer < weight[i]:
+            return answer
         else:
-            user_list[split_line[1]]=split_line[2]
-    
-    print( user_list.items())
-    for i in range(len(log_list)):
-        if log_list[i][0]=="Enter":
-            answer.append(user_list[log_list[i][1]]+"님이 들어왔습니다.")
-        else:
-            answer.append(user_list[log_list[i][1]]+"님이 나갔습니다.")
+            answer+= weight[i]
     return answer
 
-
-
-solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"])
+solution([3, 1, 6, 2, 7, 30, 1])
 
